1fbdbd5b16eb61df47ac4b1507acfcdc49510698
sukanto <mondalda@gmail.com>
2019-06-22 16:43:37 -0700

    project name argument added

--- Util/project_install.sh
+++ Util/project_install.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+    PROJECT_NAME="slim-project"
+else
+    PROJECT_NAME="$1"
+fi
+
 PROJECT_DIR=$(pwd)
 
 cd ${PROJECT_DIR}
 
 
 #The directory for the project
-mkdir -p slim-project/src/public
+mkdir -p ${PROJECT_NAME}/src/public
 
 # PHP composer install
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
-mv composer.phar ${PROJECT_DIR}/src/composer
+mv composer.phar ${PROJECT_DIR}/${PROJECT_NAME}/src/composer.phar
 
 #run composer to install slim framework
-(cd slim-project/src && php ${PROJECT_DIR}/src/composer require slim/slim)
+(cd slim-project/src && php ${PROJECT_DIR}/${PROJECT_NAME}/src/composer.phar require slim/slim)
 
 echo "$(tput setaf 2)Creating project skelton ...$(tput sgr 0)"
 
-touch slim-project/src/public/index.php
+touch ${PROJECT_NAME}/src/public/index.php
 
 
 #index file templet content 
-cat <<EOF > slim-project/src/public/index.php
+cat <<EOF > ${PROJECT_NAME}/src/public/index.php
 <?php
 use \\Psr\\Http\\Message\\ServerRequestInterface as Request;
 use \\Psr\\Http\\Message\\ResponseInterface as Response;
@@ -39,9 +45,9 @@ require '../vendor/autoload.php';
 EOF
 
 # creating .htaccs file
-touch slim-project/src/public/.htaccess
+touch ${PROJECT_NAME}/src/public/.htaccess
 
-cat <<EOF > slim-project/src/public/.htaccess
+cat <<EOF > ${PROJECT_NAME}/src/public/.htaccess
 RewriteEngine on
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteCond %{REQUEST_FILENAME} !-f
@@ -54,7 +60,7 @@ echo "$(tput setaf 2)Configuring apache...$(tput sgr 0)"
 # configure apache to run api
 
 cat <<EOF >> /etc/httpd/conf/httpd.conf
-<Directory "${PROJECT_DIR}/slim-project/src/public">
+<Directory "${PROJECT_DIR}/${PROJECT_NAME}/src/public">
     Options Indexes MultiViews FollowSymLinks
     AllowOverride All
     Order allow,deny
@@ -62,7 +68,7 @@ cat <<EOF >> /etc/httpd/conf/httpd.conf
 </Directory>
 
 <VirtualHost *:80>
-    DocumentRoot ${PROJECT_DIR}/slim-project/src/public
+    DocumentRoot ${PROJECT_DIR}/${PROJECT_NAME}/src/public
 </VirtualHost>
 EOF
 
